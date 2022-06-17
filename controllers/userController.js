7f22ab8987a23b30f69d73a5d468979a63ce0b56
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-17 12:58:53 +0530

    Store Users in MongoDB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const { body, validationResult } = require('express-validator');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 module.exports.registerValidators = [
     body("name").not().isEmpty().trim().withMessage("Name is Required"),
@@ -6,13 +11,41 @@ module.exports.registerValidators = [
     body("password").isLength({min: 6}).withMessage("Password minimum six chars long"),
 ];
 
-module.exports.register = (req, res) => {
+module.exports.register = async(req, res) => {
     const { name, email, password } = req.body;
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        res.json(errors.array())
-    } else {
-        res.json('You have register successfully');
+        return res.status(400).json({ errors: errors.array() });
     }
-}
+
+    try {
+        const checkUser = await User.findOne({ email })
+
+        if (checkUser) {
+            return res.status(400).json({ errors: [{ msg: 'Email is already taken' }] });
+        }
+        
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        try {
+            const user = await User.create({
+                name,
+                email,
+                password : hash,
+            });
+            const token = jwt.sign({ user }, process.env.SECRET, {
+              expiresIn : '7d'
+            });
+            return res.status(200).json({msg: "Your account has been created", token})
+        } catch (error) {
+        return res.status(500).json({ errors: error });
+    }
+
+    } catch (error) {
+        return res.status(500).json({ errors: error });
+    }
+
+};   
