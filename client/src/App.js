79ba2d6ca31da24fc50a16fe51fb4a8d87ffdd27
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-18 12:21:13 +0530

    React Routing & SCSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/Home';
+import './main.scss';
 
 
 function App() {
-  return <div className="App">
-    <h1>Hello Blogger</h1>
-  </div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={ Login } />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
