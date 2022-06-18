beabddf8c9f2b5ecf5d94b517814eb1ac4275af7
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-18 17:28:36 +0530

    Create Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Home from './components/Home';
 import './main.scss';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/register" exact component={Register} />
